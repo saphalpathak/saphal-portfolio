4e5aa1848507fb1b5baf98ffd16b9061fb0fa2c0
saphal.pathak2 <saphal.pathak@codehimalaya.tech>
2023-07-27 22:21:55 +0545

    initial commit

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Saphal Pathak",
+		name: "Saphal Pathak",
+		email: "saphalpathak3@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/saphalpathak",
+		linkedin: "https://www.linkedin.com/in/saphalpathak/",
+		instagram: "https://www.instagram.com/saphal_pathak/",
+	},
+
+	homepage: {
+		title: "Backend developer",
+		description:
+			"I am a backend developer with expertise in Java Spring Boot. I have experience in building scalable, secure, and reliable web applications using various Spring Boot frameworks and technologies. I enjoy solving complex problems and learning new skills in the Java ecosystem. I am passionate about creating high-quality code that adheres to best practices and industry standards in Java development. I am always seeking new challenges and opportunities to grow as a developer, particularly in the context of Java Spring Boot.",
+	},
+
+	about: {
+		title: "I’m Saphal Pathak",
+		description:
+			`I have worked on a variety of Spring Boot projects over the years, including government projects, mobile finance systems, and anti-money laundering solutions. I take pride in the progress I've made and the high-quality, scalable applications I've developed using Spring Boot.
+			I am always open to new ideas and innovations, especially in the context of Spring Boot development. If you have any thoughts on how to enhance or improve any of the Spring Boot projects I've worked on, I would be more than happy to discuss and learn from your expertise.
+			
+			In summary, I have significant experience in Spring Boot and have been involved in diverse projects, including government, mobile finance, and anti-money laundering domains. Although my projects are not open-source, I am eager to collaborate and welcome any suggestions or feedback to further improve my work.`,
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
