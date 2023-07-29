aed716650227a1314e932d6a12a1ea64a7486860
bipin321 <beepinpathak0721@gmail.com>
2023-07-29 13:12:45 +0545

    State management

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -6,26 +6,27 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 import "./styles/project.css";
 
 const Project = (props) => {
-	const { logo, title, description, linkText, link , hasLink } = props;
+	const { logo, title, description, linkText, link, hasLink } = props;
 
 	return (
 		<React.Fragment>
 			<div className="project">
-				<Link to={link} target="_blank">
-					<div className="project-container">
-						<div className="project-logo">
-							<img src={logo} alt="logo" />
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						{hasLink && <div className="project-link">
-						 <div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
-							 <div className="project-link-text">{linkText}</div> 
-						</div>
-							 }
+				<div className="project-container">
+					<div className="project-logo">
+						<img src={logo} alt="logo" />
 					</div>
+					<div className="project-title">{title}</div>
+					<div className="project-description">{description}</div>
+				</div>
+				
+				<Link to={link} target="_blank">
+					{hasLink && <div className="project-link">
+						<div className="project-link-icon">
+							<FontAwesomeIcon icon={faLink} />
+						</div>
+						<div className="project-link-text">{linkText}</div>
+					</div>
+					}
 				</Link>
 			</div>
 		</React.Fragment>
